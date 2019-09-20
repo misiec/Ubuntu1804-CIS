e19812bf119500ba903ed5ae101a7e203e3a6ef0
misiec <46483499+misiec@users.noreply.github.com>
2019-09-20 08:33:06 -0300

    Update hardening1804.sh

--- hardening1804.sh
+++ hardening1804.sh
@@ -5,4 +5,4 @@ sudo apt-get update
 sudo apt-get install ansible
 sudo apt-get install git
 sudo git clone https://github.com/misiec/Ubuntu1804-CIS /etc/roles/ubuntu-18.04
-sudo ansible-playbook -b -i "localhost," -c local /etc/roles/playbook.yml >> ansible_report
+sudo ansible-playbook -b -i "localhost," -c local /etc/roles/ubuntu-18.04/playbook.yml >> ansible_report
